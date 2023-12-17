cc5b6f4c441cb05380af40436d2a2af719087dc3
Abrham-m <abrhammen14@gmail.com>
2023-12-17 12:51:04 +0300

    asynchronous endpoints are added

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -10,8 +11,8 @@ public_users.post("/register", (req, res) => {
     if (isValid(username)) {
       users.push({ username: username, password: password });
       res.send(`${username} is successfully registered`);
-    }else{
-    res.send("username is already taken");
+    } else {
+      res.send("username is already taken");
     }
   } else {
     res.send("username or password is missing");
@@ -26,7 +27,7 @@ public_users.get("/", function (req, res) {
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
   const isbn = req.params.isbn;
-  res.status(300).send(JSON.stringify(books[isbn],null,4));
+  res.status(300).send(JSON.stringify(books[isbn], null, 4));
 });
 
 // Get book details based on author
@@ -55,7 +56,81 @@ public_users.get("/title/:title", function (req, res) {
 public_users.get("/review/:isbn", function (req, res) {
   const isbn = req.params.isbn;
   const review = books[isbn]["reviews"];
-  res.status(300).send(JSON.stringify(review,null,4));
+  res.status(300).send(JSON.stringify(review, null, 4));
+});
+
+// to get all the book available in the shop using promise or async/await
+// Task 10
+public_users.get("/get-all-book", function (req, res) {
+  let getListOfBooks = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if(books){
+      resolve(books);
+      }
+      else{
+        reject('No Book is available in the store')
+      }
+    }, 5000);
+  });
+  getListOfBooks.then((result) => {
+    res.send(JSON.stringify(result, null, 4));
+  }).catch(error => res.send(error));
+});
+
+// Get book details based on ISBN using promise or async/await
+// Task 11
+public_users.get("/get-by-isbn/:isbn", function (req, res) {
+  const isbn = req.params.isbn;
+  let getBookByIsbn = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (books[isbn]) {
+        resolve(books[isbn]);
+      } else {
+        reject("book is not found");
+      }
+    }, 5000);
+  });
+  getBookByIsbn
+    .then((result) => res.send(JSON.stringify(result, null, 4)))
+    .catch((error) => res.send(error));
+});
+
+// Get book details based on Author using promise or async/await
+// Task 12
+public_users.get("/get-by-author/:author", function (req, res) {
+  const author = req.params.author;
+  let getBookByAuthor = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      for (let book in books) {
+        if (books[book]["author"] === author) {
+          resolve(books[book]);
+        }
+      }
+      reject("book is not found");
+    }, 5000);
+  });
+  getBookByAuthor
+    .then((result) => res.send(JSON.stringify(result, null, 4)))
+    .catch((error) => res.send(error));
+});
+
+// Get book details based on title using promise or async/await
+// Task 13
+public_users.get("/get-by-title/:title", function (req, res) {
+  const title = req.params.title;
+  let getBookByTitle = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      for (let book in books) {
+        if (books[book]["title"] === title) {
+          resolve(books[book]);
+        }
+      }
+      reject("book is not found");
+    }, 5000);
+  });
+  getBookByTitle
+    .then((result) => res.send(JSON.stringify(result, null, 4)))
+    .catch((error) => res.send(error));
 });
 
 module.exports.general = public_users;
